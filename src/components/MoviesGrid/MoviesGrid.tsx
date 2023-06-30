d4ead737168491b5c8e565b49fe775b124a19dc4
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-06-30 19:21:51 +0100

    Add component MovieGrid

--- src/components/MoviesGrid/MoviesGrid.tsx
+++ src/components/MoviesGrid/MoviesGrid.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import MovieCard from "../MovieCard/MovieCard";
-import {Movie} from "../../../types/Movie";
+import {Movie} from "../../types/Movie";
 
 const MoviesGrid = ({movies}: { movies: Movie[] }) => {
+    const moviesFound = movies.length > 0;
+
+    return (
+        moviesFound
+            ? <MoviesFound movies={movies}/>
+            : <NoMoviesFound/>
+    );
+}
+
+const MoviesFound = ({movies}: { movies: Movie[] }) => {
     return (
         <Movies>
             {movies.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
         </Movies>
     );
 }
+
+const NoMoviesFound = () => {
+    return (
+        <NoMovies>
+            No movies found for this search
+        </NoMovies>
+    );
+}
+
+const NoMovies = styled.div`
+  color: var(--color-white);
+  font-size: clamp(1.2rem, 4vw, 1.5rem);
+  margin-bottom: 1rem;
+`;
 
 const Movies = styled.div`
   display: grid;
